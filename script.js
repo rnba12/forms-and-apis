ab0e005a1b57d09be7dcd3ec329b2ab5a4d74bdf
rnba12 <rnba12@yahoo.co.uk>
2022-11-09 14:44:39 +0000

    added images to card selector

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ function createCard(e) {
     let message = document.getElementById("message").value;
     myForm.style.display = "none";
     document.getElementById("card").style.display = "block"
-    document.getElementById("cardGreeting").textContent = greeting;
+    document.getElementById("cardGreeting").textContent = `Dear ${greeting}`;
     document.getElementById("cardEvent").textContent = event;
     document.getElementById("cardMessage").textContent = message;
+    console.log(event);
+    if (event == "Happy Birthday!") {
+        document.getElementById("bday").style.display = "block";
+    } else if (event == "Merry Christmas!") {
+        document.getElementById("xmas").style.display = "block";
+    } else if (event == "Get Well Soon!") {
+        document.getElementById("getwell").style.display = "block";
+    } else if (event == "Congratulations!") {
+        document.getElementById("congrats").style.display = "block";
+    }
 }
