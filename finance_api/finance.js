247e2c9701482ca7fe0ab5218391fe30e375534c
“liambrockpy” <liambrock72@gmail.com>
2022-11-09 17:30:22 +0000

    Update currency list based on selected option

--- finance_api/finance.js
+++ finance_api/finance.js
@@ -1,13 +1,30 @@
-const selected = document.querySelector('#currency')
 const arr = ["EUR", "GBP", "AUD", "NZD", "USD", "CAD", "CHF", "JPY"]
+const selected = document.querySelector('#currency')
+const list = document.querySelector('#currency-list')
 
-let selectedCurrency;
+let selectedCurrency
 let newArr
 
 selected.addEventListener('change', (e) => {
+    list.innerHTML = ""
     selectedCurrency = e.target.value
     newArr = arr.filter((curr) => curr !== selectedCurrency)
-    // let children = newArr.map(el => document.createElement('h6').innerHTML(el))
-    // console.log(children)
-    // document.querySelector('#currency-list').appendChild(() => 
+    showCurrencies(newArr)
 })
+
+// spans created will have ids of 'c1' to 'c7', 
+// can be used to query and update with rates fetched from api
+function showCurrencies(arr) {
+    let children = arr.map((currency, index) => {
+        let parent = document.createElement('h6')
+        let child = document.createElement('span')
+        child.setAttribute('id', `c${index + 1}`)
+        parent.textContent = currency
+        parent.appendChild(child)
+        return parent
+    })
+
+    children.forEach(child => {
+        list.appendChild(child)
+    })
+}
